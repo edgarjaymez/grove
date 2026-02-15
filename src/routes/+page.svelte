7e1f7601cf24ecefc6aa17abbd4d46e604f7f29b
ediazjz <iam.ediazjz@gmail.com>
2026-02-15 13:27:56 -0600

    feat: add Icon component

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,13 @@
+<script>
+	import Icon from '$lib/components/Icon.svelte';
+</script>
+
 <main>
 	<h1>Welcome to your library project</h1>
 	<p>Create your package using @sveltejs/package and preview/showcase your work with SvelteKit</p>
 	<p>
 		Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation
 	</p>
+
+	<Icon unicode="" lineHeight="multiline" textStyle="base" fillInHover />
 </main>
