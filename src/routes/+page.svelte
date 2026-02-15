c6fccc039bb9c8182ae1607fac7687057ddf7938
ediazjz <iam.ediazjz@gmail.com>
2026-02-15 15:02:37 -0600

    chore: experiment with a basic version of the hero

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,21 +1,60 @@
-<script>
-	import Icon from '$lib/components/Icon.svelte';
+<script lang="ts">
+	import Isotype from '$lib/components/Isotype.svelte';
+
+	const version = __APP_VERSION__;
 </script>
 
 <main>
-	<div class="title-container">
-		<Icon unicode="" fillInHover class="display singleline" />
-		<h1 class="display singleline">Welcome to your library project</h1>
-	</div>
-	<p>Create your package using @sveltejs/package and preview/showcase your work with SvelteKit</p>
-	<p>
-		Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation
-	</p>
+	<header>
+		<h1 class="hero singleline">Grove</h1>
+		<p class="display singleline">Where living systems take root</p>
+		<span class="title singleline"
+			>By <small class="title singleline emphasis">Edgar Diaz</small></span
+		>
+	</header>
+
+	<data class="display singleline" value={version}>{version}</data>
+
+	<Isotype class="isotype" color="base" size={1000} tone="dark" />
 </main>
 
 <style>
-	.title-container {
+	main {
+		height: calc(100svh - var(--soft-grid-32) * 2);
+		margin: var(--soft-grid-32);
+		padding: var(--grid-landing-margin);
+		border-radius: var(--border-radius-12xl);
+		background-color: var(--semantic-color-surface-brand-summit);
+		color: var(--semantic-color-text-on-brand-summit-base);
 		display: flex;
-		gap: 1rem;
+		flex-direction: column;
+		justify-content: space-between;
+		position: relative;
+		overflow: hidden;
+
+		header {
+			display: flex;
+			flex-direction: column;
+			gap: var(--soft-grid-32);
+
+			p,
+			span {
+				color: var(--semantic-color-text-on-brand-summit-subtle);
+			}
+		}
+
+		data {
+			margin-left: auto;
+			color: var(--semantic-color-text-on-brand-summit-base);
+			text-align: right;
+			z-index: 1;
+		}
+
+		:global(.isotype) {
+			position: absolute;
+			right: -15rem;
+			bottom: -13.5rem;
+			z-index: 0;
+		}
 	}
 </style>
