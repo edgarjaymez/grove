080841c308c357ac191e72702d1e6c55f77336ca
ediazjz <iam.ediazjz@gmail.com>
2026-02-14 19:04:34 -0600

    feat: set up permutations correctly

--- terrazzo.config.js
+++ terrazzo.config.js
@@ -2,12 +2,82 @@ import { defineConfig } from '@terrazzo/cli';
 import css from '@terrazzo/plugin-css';
 
 export default defineConfig({
+	tokens: ['./src/lib/tokens/main.resolver.json'],
+	outDir: './src/lib/tokens',
+
 	plugins: [
 		css({
 			filename: 'tokens.css',
-			variableName: (token) => token.id.replace(/\./g, '-'),
-			baseSelector: ':root',
-			baseScheme: 'light' // Optional: support both light and dark themes
+			// variableName: (token) => token.id.replace(/\./g, '-'),
+			permutations: [
+				{
+					input: {}, // default
+					prepare: (css) =>
+						`:root {
+							color-scheme: light dark;
+							${css}
+						}`
+				},
+				{
+					input: { theme: 'light' },
+					prepare: (css) =>
+						`[data-theme="light"] {
+							color-scheme: light;
+							${css}
+						}`
+				},
+				{
+					input: { theme: 'dark' },
+					prepare: (css) =>
+						`@media (prefers-color-scheme: dark) {
+							:root {
+								color-scheme: dark;
+								${css}
+							}
+						}
+
+						[data-theme="dark"] {
+							color-scheme: dark;
+							${css}
+						}`
+				},
+				{
+					input: { breakpoint: 'tablet' },
+					prepare: (css) =>
+						`@media (width >= 768px) {
+							:root {
+								${css}
+							}
+						}`
+				},
+				{
+					input: { breakpoint: 'laptop' },
+					prepare: (css) =>
+						`@media (width >= 1280px) {
+							:root {
+								${css}
+							}
+						}`
+				},
+				{
+					input: { breakpoint: 'desktop' },
+					prepare: (css) =>
+						`@media (width >= 1536px) {
+							:root {
+								${css}
+							}
+						}`
+				},
+				{
+					input: { media: 'print' },
+					prepare: (css) =>
+						`@media print {
+							:root {
+								${css}
+							}
+						}`
+				}
+			]
 		})
 	]
 });
