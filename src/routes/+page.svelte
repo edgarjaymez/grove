df2f7ecbcec4774f08a188c3e743af254bd746ac
ediazjz <iam.ediazjz@gmail.com>
2026-02-15 00:18:04 -0600

    feat: add global.css

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -5,10 +5,3 @@
 		Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation
 	</p>
 </main>
-
-<style>
-	main {
-		background-color: var(--semantic-color-surface-ground);
-		margin: 0 var(--grid-system-margin);
-	}
-</style>
