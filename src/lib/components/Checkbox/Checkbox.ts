f14feec9905f3cc68c785ac90bd40a3b2ca85809
edgarjaymez <iam.ediazjz@gmail.com>
2026-06-25 07:36:18 -0600

    fix: Figma interface

--- src/lib/components/Checkbox/Checkbox.ts
+++ src/lib/components/Checkbox/Checkbox.ts
@@ -11,75 +11,78 @@ export class Checkbox extends LitElement {
 	@property({ type: String }) responsive: CheckboxResponsive = 'default';
 	@property({ type: Boolean, reflect: true }) disabled = false;
 
-	static styles = [componentReset, css`
-		.checkbox {
-			position: relative;
-			display: inline-flex;
-			align-items: center;
-			justify-content: center;
-			overflow: hidden;
-			flex-shrink: 0;
-			width: 24px;
-			height: 24px;
-			border-radius: var(--border-radius-sm);
-			border: var(--border-width-base) solid var(--color-gray-800);
-			background: var(--semantic-color-surface-ground);
-			color: transparent;
-			cursor: pointer;
-			transition:
-				background-color 200ms,
-				border-color 200ms,
-				color 200ms;
-		}
+	static styles = [
+		componentReset,
+		css`
+			.checkbox {
+				position: relative;
+				display: inline-flex;
+				align-items: center;
+				justify-content: center;
+				overflow: hidden;
+				flex-shrink: 0;
+				width: 24px;
+				height: 24px;
+				border-radius: var(--border-radius-sm);
+				border: var(--border-width-base) solid var(--color-gray-800);
+				background: var(--semantic-color-surface-ground);
+				color: transparent;
+				cursor: pointer;
+				transition:
+					background-color 200ms,
+					border-color 200ms,
+					color 200ms;
+			}
 
-		.checkbox.xl {
-			width: 28px;
-			height: 28px;
-		}
+			.checkbox.xl {
+				width: 28px;
+				height: 28px;
+			}
 
-		.checkbox[aria-checked='true'] {
-			background: var(--semantic-color-surface-brand-summit);
-			border-color: var(--semantic-color-surface-brand-summit);
-			color: var(--semantic-color-text-on-brand-summit-base);
-		}
+			.checkbox[aria-checked='true'] {
+				background: var(--semantic-color-surface-brand-summit);
+				border-color: var(--semantic-color-surface-brand-summit);
+				color: var(--semantic-color-text-on-brand-summit-base);
+			}
 
-		.checkbox:hover:not([aria-checked='true']):not([disabled]) {
-			border-color: var(--semantic-color-border-around-gray-aurora);
-		}
+			.checkbox:hover:not([aria-checked='true']):not([disabled]) {
+				border-color: var(--semantic-color-border-around-gray-aurora);
+			}
 
-		.checkbox:hover[aria-checked='true']:not([disabled]) {
-			background: var(--color-brand-600);
-			border-color: var(--color-brand-600);
-			color: var(--semantic-color-text-on-brand-aurora-base);
-		}
+			.checkbox:hover[aria-checked='true']:not([disabled]) {
+				background: var(--color-brand-600);
+				border-color: var(--color-brand-600);
+				color: var(--semantic-color-text-on-brand-aurora-base);
+			}
 
-		.checkbox:active[aria-checked='true']:not([disabled]) {
-			background: var(--semantic-color-surface-brand-aurora);
-			border-color: var(--semantic-color-surface-brand-aurora);
-			color: var(--semantic-color-text-on-brand-aurora-base);
-		}
+			.checkbox:active[aria-checked='true']:not([disabled]) {
+				background: var(--semantic-color-surface-brand-aurora);
+				border-color: var(--semantic-color-surface-brand-aurora);
+				color: var(--semantic-color-text-on-brand-aurora-base);
+			}
 
-		.checkbox[disabled] {
-			background: var(--color-gray-50);
-			border-color: var(--color-gray-800);
-			cursor: not-allowed;
-			opacity: 0.5;
-		}
+			.checkbox[disabled] {
+				background: var(--color-gray-50);
+				border-color: var(--color-gray-800);
+				cursor: not-allowed;
+				opacity: 0.5;
+			}
 
-		.checkbox[disabled][aria-checked='true'] {
-			color: var(--semantic-color-text-on-gray-summit-subtle);
-		}
+			.checkbox[disabled][aria-checked='true'] {
+				color: var(--semantic-color-text-on-gray-summit-subtle);
+			}
 
-		.check {
-			width: 12px;
-			height: 8.862px;
-		}
+			.check {
+				width: 12px;
+				height: 8.862px;
+			}
 
-		.checkbox.xl .check {
-			width: 16px;
-			height: 11.816px;
-		}
-	`];
+			.checkbox.xl .check {
+				width: 16px;
+				height: 11.816px;
+			}
+		`
+	];
 
 	private _toggle() {
 		this.checked = !this.checked;
